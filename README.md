1ce5e2312f829624c91e65d6dbc85ede0b48e77a
Wakaru Himura <wakaru44@gmail.com>
2016-01-24 00:42:30 +0100

    added files from the old repo

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# A/B Testing API
+
+A ultra simplistic RESTfake (Rest in your own grave) API for a dirty little experiment
+
+TODO'S:
+
+- termina los metodos para validar input y demas
+- empezar a pensar en a donde mandar los mensajes.
---
+++ abapi/helpers.py
@@ -0,0 +1,20 @@
+import string
+
+def sessionid_check(sesid):
+  """
+  validate that this session id is correct
+  """
+  return string(sesid)
+
+def stamp_check(stamp):
+  """
+  validatethat this is a timestamp
+  """
+  return 100000
+
+def client_check(client):
+  pass
+
+def item_check(item):
+  pass
+
