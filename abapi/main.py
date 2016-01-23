3dd02ba471cf1e8daad9b8968b4263c5ff050709
Wakaru Himura <wakaru44@gmail.com>
2016-01-24 00:50:24 +0100

    general cleanup of boileplate debris

--- abapi/main.py
+++ abapi/main.py
@@ -29,32 +29,10 @@ def event_listener(whatever):
 
 @app.route('/')
 def index():
-    return "You are at the homepage!"
+    return render_template("base.html")
 
-    
-
-@app.route('/redirect-to-<function>')
-def pointless_redirect(function=None):
-    return redirect(url_for(function))
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-
-@app.route('/login', methods=['GET', 'POST'])
-def login_handler():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                        request.form['password']):
-            return log_user_in(request.form['username'])
-        else:
-            error = "Invalid username/password!"
-    return render_template('login.html', error=error)
-
 
